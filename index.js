c0c5ee14416d5fb1e143daf111183efc58e27685
Henry Oswald <henry.oswald@sharelatex.com>
2014-03-26 16:20:08 +0000

    coffeescirpt != js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var i18n = require("i18next");
-i18n.init(resGetPath: 'locales/__lng__.json')
+i18n.init({resGetPath: 'locales/__lng__.json'})
 
 module.exports = {
     expressMiddlewear: i18n.handle
