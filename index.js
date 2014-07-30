1bee6ae5d154898bd9cd2a6e1828b4ce4384274f
Henry Oswald <henry.oswald@sharelatex.com>
2014-07-30 14:19:16 +0100

    added function to serve locals file

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var i18n = require("i18next");
+var fs = require("fs")
+
 i18n.init({resGetPath: __dirname+'/locales/__lng__.json'})
 module.exports = {
-      expressMiddlewear: i18n.handle
-}
+      expressMiddlewear: i18n.handle,
+      serverStaticFiles: function(req, res, next){
+      		if(req.url.indexOf("/locales") == 0){
+      			res.sendfile(__dirname+req.url)
+      		} else {
+	      		next()
 
+      		}
+      }
+}
