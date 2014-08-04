c1e0d2f509feca5ad63b6467776cdf9dbde8c3a6
Henry Oswald <henry.oswald@sharelatex.com>
2014-08-04 11:56:09 +0100

    save missing keys in file missing-lang.json, the missing go to the bottom of the file

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var i18n = require("i18next");
 var fs = require("fs")
 
-i18n.init({resGetPath: __dirname+'/locales/__lng__.json'})
+i18n.init(
+	{
+		resGetPath: __dirname+'/locales/__lng__.json',
+		saveMissing: true,
+		resSetPath: __dirname+'/locales/missing-__lng__.json',
+		sendMissingTo: 'fallback',
+		fallbackLng:"en"
+	}
+)
+
 module.exports = {
       expressMiddlewear: i18n.handle,
       serverStaticFiles: function(req, res, next){
