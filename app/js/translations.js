c5d996c77dc4a5d35c67f4b3f3118a86d8d99825
Jakob Ackermann <das7pad@outlook.com>
2019-08-31 17:10:51 +0200

    [misc] update compiled artifact
    Signed-off-by: Jakob Ackermann <das7pad@outlook.com>

--- app/js/translations.js
+++ app/js/translations.js
@@ -1,9 +1,11 @@
 (function() {
-  var i18n, _;
+  var i18n, path, _;
 
   i18n = require("i18next");
 
   _ = require("underscore");
+
+  path = require("path");
 
   module.exports = {
     setup: function(options) {
@@ -11,9 +13,9 @@
       subdomainLang = (options != null ? options.subdomainLang : void 0) || {};
       availableLngs = _.pluck(_.values(subdomainLang), "lngCode");
       i18n.init({
-        resGetPath: __dirname + "/locales/__lng__.json",
+        resGetPath: path.resolve(__dirname, "../../", "locales/__lng__.json"),
         saveMissing: true,
-        resSetPath: __dirname + "/locales/missing-__lng__.json",
+        resSetPath: path.resolve(__dirname, "../../", "locales/missing-__lng__.json"),
         sendMissingTo: "fallback",
         fallbackLng: (options != null ? options.defaultLng : void 0) || "en",
         detectLngFromHeaders: true,
@@ -22,15 +24,13 @@
         supportedLngs: availableLngs
       });
       setLangBasedOnDomainMiddlewear = function(req, res, next) {
-        var host, lang, subdomain, _ref, _ref1;
+        var host, lang, parts, subdomain, _ref, _ref1;
         host = req.headers.host;
         if (host == null) {
           return next();
         }
-        subdomain = host.slice(0, host.indexOf("."));
-        if (subdomain == null) {
-          return next();
-        }
+        parts = host.split(/[.-]/);
+        subdomain = parts[0];
         lang = options != null ? (_ref = options.subdomainLang) != null ? (_ref1 = _ref[subdomain]) != null ? _ref1.lngCode : void 0 : void 0 : void 0;
         if (req.originalUrl.indexOf("setLng") === -1 && (lang != null)) {
           req.i18n.setLng(lang);
