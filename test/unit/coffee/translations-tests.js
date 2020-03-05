c924a03de45124022fb9df60ffffe95c8aee69ed
decaffeinate <mserranom@gmail.com>
2020-03-05 17:13:54 +0100

    decaffeinate: Convert translations-tests.coffee to JS

--- test/unit/coffee/translations-tests.js
+++ test/unit/coffee/translations-tests.js
@@ -1,61 +1,86 @@
-should = require('chai').should()
-SandboxedModule = require('sandboxed-module')
-assert = require('assert')
-path = require('path')
-sinon = require('sinon')
-modulePath = path.join __dirname, "../../../app/js/translations.js"
-expect = require("chai").expect
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const should = require('chai').should();
+const SandboxedModule = require('sandboxed-module');
+const assert = require('assert');
+const path = require('path');
+const sinon = require('sinon');
+const modulePath = path.join(__dirname, "../../../app/js/translations.js");
+const { expect } = require("chai");
 
-describe "translations", ->
+describe("translations", function() {
 
-	beforeEach ->
+	beforeEach(function() {
 
-		@settings = {}
-		@translations = SandboxedModule.require modulePath, requires:
-			"settings-sharelatex":@settings
-			"logger-sharelatex": log:->
+		this.settings = {};
+		this.translations = SandboxedModule.require(modulePath, { requires: {
+			"settings-sharelatex":this.settings,
+			"logger-sharelatex": { log() {}
+		}
+		}
+	}
+		);
 
-		opts = 
-			subdomainLang:
-				www: {lngCode:"en", url: "www.sharelatex.com"}
-				fr: {lngCode:"fr", url: "fr.sharelatex.com"}
+		const opts = { 
+			subdomainLang: {
+				www: {lngCode:"en", url: "www.sharelatex.com"},
+				fr: {lngCode:"fr", url: "fr.sharelatex.com"},
 				da: {lngCode:"da", url: "da.sharelatex.com"}
-		@translations = @translations.setup(opts)
-		@req =
-			originalUrl:"doesn'tmatter.sharelatex.com/login"
-			headers:
+			}
+		};
+		this.translations = this.translations.setup(opts);
+		this.req = {
+			originalUrl:"doesn'tmatter.sharelatex.com/login",
+			headers: {
 				"accept-language":""
-		@res = {}
+			}
+		};
+		return this.res = {};});
 
 
-	describe "setLangBasedOnDomainMiddlewear", ->
+	return describe("setLangBasedOnDomainMiddlewear", function() {
 
-		it "should set the lang to french if the domain is fr", (done)->
-			@req.url = "fr.sharelatex.com/login"
-			@req.headers.host = "fr.sharelatex.com"
-			@translations.expressMiddlewear @req, @req, =>
-				@translations.setLangBasedOnDomainMiddlewear @req, @res, =>
-					@req.lng.should.equal "fr"
-					done()
-
-
-		describe "showUserOtherLng", ->
-
-			it "should set it to true if the languge based on headers is different to lng", (done)->
-				@req.headers["accept-language"] = "da, en-gb;q=0.8, en;q=0.7"
-				@req.url = "fr.sharelatex.com/login"
-				@req.headers.host = "fr.sharelatex.com"
-				@translations.expressMiddlewear @req, @req, =>
-					@translations.setLangBasedOnDomainMiddlewear @req, @res, =>
-						@req.showUserOtherLng.should.equal "da"
-						done()
+		it("should set the lang to french if the domain is fr", function(done){
+			this.req.url = "fr.sharelatex.com/login";
+			this.req.headers.host = "fr.sharelatex.com";
+			return this.translations.expressMiddlewear(this.req, this.req, () => {
+				return this.translations.setLangBasedOnDomainMiddlewear(this.req, this.res, () => {
+					this.req.lng.should.equal("fr");
+					return done();
+				});
+			});
+		});
 
 
-			it "should not set prop", (done)->
-				@req.headers["accept-language"] = "da, en-gb;q=0.8, en;q=0.7"
-				@req.url = "da.sharelatex.com/login"
-				@req.headers.host = "da.sharelatex.com"
-				@translations.expressMiddlewear @req, @req, =>
-					@translations.setLangBasedOnDomainMiddlewear @req, @res, =>
-						expect(@req.showUserOtherLng).to.not.exist
-						done()
+		return describe("showUserOtherLng", function() {
+
+			it("should set it to true if the languge based on headers is different to lng", function(done){
+				this.req.headers["accept-language"] = "da, en-gb;q=0.8, en;q=0.7";
+				this.req.url = "fr.sharelatex.com/login";
+				this.req.headers.host = "fr.sharelatex.com";
+				return this.translations.expressMiddlewear(this.req, this.req, () => {
+					return this.translations.setLangBasedOnDomainMiddlewear(this.req, this.res, () => {
+						this.req.showUserOtherLng.should.equal("da");
+						return done();
+					});
+				});
+			});
+
+
+			return it("should not set prop", function(done){
+				this.req.headers["accept-language"] = "da, en-gb;q=0.8, en;q=0.7";
+				this.req.url = "da.sharelatex.com/login";
+				this.req.headers.host = "da.sharelatex.com";
+				return this.translations.expressMiddlewear(this.req, this.req, () => {
+					return this.translations.setLangBasedOnDomainMiddlewear(this.req, this.res, () => {
+						expect(this.req.showUserOtherLng).to.not.exist;
+						return done();
+					});
+				});
+			});
+		});
+	});
+});
