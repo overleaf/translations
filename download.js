8d42aef224d67e97dca4f02093a24ca6954fc6c4
Alasdair Smith <ali@alasdairsmith.co.uk>
2020-07-24 13:00:16 +0100

    Switch to using @brainly/onesky-utils package
    The old onesky package has been deprecated, so we needed to switch. The
    new package also uses promises, so converted to use async/await too.

--- download.js
+++ download.js
@@ -1,8 +1,5 @@
 const fs = require('fs')
-const publicKey = process.env.ONE_SKY_PUBLIC_KEY
-const privateKey = process.env.ONE_SKY_PRIVATE_KEY
-
-const onesky = require('onesky')(publicKey, privateKey)
+const oneSky = require('@brainly/onesky-utils')
 const sanitizeHtml = require('sanitize-html')
 const sanitizeOpts = {
   allowedTags: ['b', 'strong', 'a', 'code'],
@@ -14,27 +11,38 @@ const sanitizeOpts = {
   }
 }
 
-onesky.string.output({ platformId: '25049' }, function(err, r) {
-  if (err) {
-    console.error(err)
-    return
-  }
+async function download() {
+  try {
+    const content = await oneSky.getMultilingualFile({
+      apiKey: process.env.ONE_SKY_PUBLIC_KEY,
+      secret: process.env.ONE_SKY_PRIVATE_KEY,
+      projectId: '25049',
+      fileName: 'en-US.json'
+    })
+    const json = JSON.parse(content)
 
-  for (const [code, lang] of Object.entries(r.translation['en-US.json'])) {
-    for (const [key, value] of Object.entries(lang)) {
-      lang[key] = sanitizeHtml(value, sanitizeOpts)
+    for (const [code, lang] of Object.entries(json)) {
+      for (const [key, value] of Object.entries(lang.translation)) {
+        lang.translation[key] = sanitizeHtml(value, sanitizeOpts)
+      }
+      fs.writeFileSync(
+        `./locales/${code}.json`,
+        JSON.stringify(lang.translation, null, 2)
+      )
     }
 
-    fs.writeFileSync(`./locales/${code}.json`, JSON.stringify(lang, null, 2))
+    fs.createReadStream('./locales/en-GB.json').pipe(
+      fs.createWriteStream('./locales/en-US.json')
+    )
+    fs.createReadStream('./locales/en-GB.json').pipe(
+      fs.createWriteStream('./locales/en.json')
+    )
+    fs.createReadStream('./locales/zh-CN.json').pipe(
+      fs.createWriteStream('./locales/cn.json')
+    )
+  } catch (error) {
+    console.error(error)
   }
+}
 
-  fs.createReadStream('./locales/en-GB.json').pipe(
-    fs.createWriteStream('./locales/en-US.json')
-  )
-  fs.createReadStream('./locales/en-GB.json').pipe(
-    fs.createWriteStream('./locales/en.json')
-  )
-  return fs
-    .createReadStream('./locales/zh-CN.json')
-    .pipe(fs.createWriteStream('./locales/cn.json'))
-})
+download()
