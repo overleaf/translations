1fd7c819738cc752e49a20bbb002a679071d8456
Alasdair Smith <ali@alasdairsmith.co.uk>
2020-07-24 14:56:36 +0100

    Add comment explaining unusual OneSky setup

--- download.js
+++ download.js
@@ -4,6 +4,12 @@ const sanitizeHtml = require('sanitize-html')
 
 async function run() {
   try {
+    // The recommended OneSky set-up appears to require an API request to
+    // generate files on their side, which you could then request and use. We
+    // only have 1 such file that appears to be misnamed (en-US, despite our
+    // translations being marked as GB) and very out-of-date.
+    // However by requesting the "multilingual file" for this file, we get all
+    // of the translations
     const content = await oneSky.getMultilingualFile({
       apiKey: process.env.ONE_SKY_PUBLIC_KEY,
       secret: process.env.ONE_SKY_PRIVATE_KEY,
