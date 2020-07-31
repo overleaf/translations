b8a620ea2789c4a7a5489fc6677185300360bb78
Alasdair Smith <ali@alasdairsmith.co.uk>
2020-07-31 11:21:39 +0100

    Fix language mismatch flag not set correctly
    It appears that previously we were relying on a quirk of i18next, where
    the detected language (via header detection), would be set on a
    different property to the explicitly set language.
    
    In the latest version this no longer works, so switch to directly using
    the language detection to set the showUserOtherLng flag.

--- app/js/translations.js
+++ app/js/translations.js
@@ -22,6 +22,8 @@ module.exports = {
       fallbackLng: options.defaultLng || 'en'
     })
 
+    const langDetector = new middleware.LanguageDetector(i18n.services)
+
     function setLangBasedOnDomainMiddleware(req, res, next) {
       // Determine language from subdomain
       const { host } = req.headers
@@ -38,12 +40,12 @@ module.exports = {
         req.i18n.changeLanguage(lang)
       }
 
-      // If the set language (req.lng) is different from the language
-      // automatically determined by the i18next middleware (req.language), then
-      // set flag which will show a banner offering to switch to the appropriate
-      // language
-      if (req.language !== req.lng) {
-        req.showUserOtherLng = req.language
+      // If the set language is different from the language detection (based on
+      // the Accept-Language header), then set flag which will show a banner
+      // offering to switch to the appropriate library
+      const detectedLanguage = langDetector.detect(req, res)
+      if (req.language !== detectedLanguage) {
+        req.showUserOtherLng = detectedLanguage
       }
 
       next()
