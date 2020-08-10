803f2ff5293b63cf75c84b712551b3dcea4a7fec
Alasdair Smith <ali@alasdairsmith.co.uk>
2020-08-10 10:37:58 +0100

    Reorganise tests and add some cases for basic translation

--- test/unit/js/translations-tests.js
+++ test/unit/js/translations-tests.js
@@ -24,6 +24,61 @@ describe('translations', function() {
     }
   })
 
+  describe('translate', function() {
+    beforeEach(function(done) {
+      this.req.url = 'www.sharelatex.com/login'
+      this.translations.expressMiddleware(this.req, this.res, done)
+    })
+
+    it('works', function() {
+      expect(this.req.i18n.t('give_feedback')).to.equal('Give feedback')
+    })
+
+    it('has translate alias', function() {
+      expect(this.req.i18n.translate('give_feedback')).to.equal('Give feedback')
+    })
+  })
+
+  describe('interpolation', function() {
+    beforeEach(function(done) {
+      this.req.url = 'www.sharelatex.com/login'
+      this.translations.expressMiddleware(this.req, this.res, done)
+    })
+
+    it('works', function() {
+      expect(
+        this.req.i18n.t('please_confirm_email', {
+          emailAddress: 'foo@example.com'
+        })
+      ).to.equal(
+        'Please confirm your email foo@example.com by clicking on the link in the confirmation email '
+      )
+    })
+
+    it('handles dashes after interpolation', function() {
+      // This translation string has a problematic interpolation followed by a
+      // dash: `__len__-day`
+      expect(
+        this.req.i18n.t('faq_how_does_free_trial_works_answer', {
+          appName: 'Overleaf',
+          len: '5'
+        })
+      ).to.equal(
+        'You get full access to your chosen Overleaf plan during your 5-day free trial. There is no obligation to continue beyond the trial. Your card will be charged at the end of your 5 day trial unless you cancel before then. You can cancel via your subscription settings.'
+      )
+    })
+
+    it('disables escaping', function() {
+      expect(
+        this.req.i18n.t('admin_user_created_message', {
+          link: 'http://google.com'
+        })
+      ).to.equal(
+        'Created admin user, <a href="http://google.com">Log in here</a> to continue'
+      )
+    })
+  })
+
   describe('query string detection', function() {
     it('sets the language to french if the setLng query string is fr', function(done) {
       this.req.originalUrl = 'www.sharelatex.com/login?setLng=fr'
@@ -77,7 +132,7 @@ describe('translations', function() {
     })
 
     describe('showUserOtherLng', function() {
-      it('should set it to true if the language based on headers is different to lng', function(done) {
+      it('should set showUserOtherLng=true if the detected lang is different to subdomain lang', function(done) {
         this.req.headers['accept-language'] = 'da, en-gb;q=0.8, en;q=0.7'
         this.req.url = 'fr.sharelatex.com/login'
         this.req.headers.host = 'fr.sharelatex.com'
@@ -93,7 +148,7 @@ describe('translations', function() {
         })
       })
 
-      it('should not set prop', function(done) {
+      it('should not set showUserOtherLng if the detected lang is the same as subdomain lang', function(done) {
         this.req.headers['accept-language'] = 'da, en-gb;q=0.8, en;q=0.7'
         this.req.url = 'da.sharelatex.com/login'
         this.req.headers.host = 'da.sharelatex.com'
@@ -107,53 +162,6 @@ describe('translations', function() {
             }
           )
         })
-      })
-    })
-
-    describe('interpolation', function() {
-      beforeEach(function(done) {
-        this.req.url = 'www.sharelatex.com/login'
-        this.req.headers.host = 'www.sharelatex.com'
-        this.translations.expressMiddleware(this.req, this.res, () => {
-          this.translations.setLangBasedOnDomainMiddleware(
-            this.req,
-            this.res,
-            done
-          )
-        })
-      })
-
-      it('works', function() {
-        expect(
-          this.req.i18n.t('please_confirm_email', {
-            emailAddress: 'foo@example.com'
-          })
-        ).to.equal(
-          'Please confirm your email foo@example.com by clicking on the link in the confirmation email '
-        )
-      })
-
-      it('handles dashes after interpolation', function() {
-        // This translation string has a problematic interpolation followed by a
-        // dash: `__len__-day`
-        expect(
-          this.req.i18n.t('faq_how_does_free_trial_works_answer', {
-            appName: 'Overleaf',
-            len: '5'
-          })
-        ).to.equal(
-          'You get full access to your chosen Overleaf plan during your 5-day free trial. There is no obligation to continue beyond the trial. Your card will be charged at the end of your 5 day trial unless you cancel before then. You can cancel via your subscription settings.'
-        )
-      })
-
-      it('disables escaping', function() {
-        expect(
-          this.req.i18n.translate('admin_user_created_message', {
-            link: 'http://google.com'
-          })
-        ).to.equal(
-          'Created admin user, <a href="http://google.com">Log in here</a> to continue'
-        )
       })
     })
   })
